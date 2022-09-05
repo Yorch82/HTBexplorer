25bb4f46bdcc9e9e837694ef877ce0846d5e4cca
Yorch <jcampo1982@outlook.com>
2022-09-05 15:00:57 +0200

    metodos combinados

--- htbmachines.sh
+++ htbmachines.sh
@@ -26,6 +26,9 @@ function helpPanel(){
   echo -e "\t${purpleColour}u)${endColour}${grayColour} Descargar o actualizar archivos necesarios${endColour}"
   echo -e "\t${purpleColour}m)${endColour}${grayColour} Buscar por nombre de máquina${endColour}"
   echo -e "\t${purpleColour}i)${endColour}${grayColour} Buscar por dirección IP${endColour}"
+  echo -e "\t${purpleColour}y)${endColour}${grayColour} Obtener link de la resolución de la máquina en YouTube${endColour}"
+  echo -e "\t${purpleColour}d)${endColour}${grayColour} Buscar por la dificultad de una máquina${endColour}"
+  echo -e "\t${purpleColour}o)${endColour}${grayColour} Buscar por el sistema operativo${endColour}"
   echo -e "\t${purpleColour}h)${endColour}${grayColour} Mostrar este panel de ayuda${endColour}\n"
   
 }
@@ -64,9 +67,15 @@ function updateFiles(){
 
 searchMachine(){
   machineName="$1"
+   
+  machineName_checker=$(cat bundle.js | awk "/name: \"${machineName}\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta" | tr -d '"' | tr -d ',' | sed 's/^ *//')
 
-  echo -e "\n${yellowColour}[+]${endColour}${grayColour} Listando las propiedades de la máquina${endColour}${blueColour} $machineName${endColour}${grayColour}:${endColour}\n"
-  cat bundle.js | awk "/name: \"${machineName}\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta" | tr -d '"' | tr -d ',' | sed 's/^ *//'
+  if [ "$machineName_checker" ]; then
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Listando las propiedades de la máquina${endColour}${blueColour} $machineName${endColour}${grayColour}:${endColour}\n"
+    cat bundle.js | awk "/name: \"${machineName}\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta" | tr -d '"' | tr -d ',' | sed 's/^ *//'
+  else
+    echo -e "\n${redColour}[!] La máquina proporcionada no existe${endColour}"
+  fi
 }
 
 searchIP(){
@@ -74,17 +83,79 @@ searchIP(){
   
   machineName=$(cat bundle.js | grep "ip: \"$ipAddress\"" -B 3 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',')
 
-  echo -e "\n${yellowColour}[+]${endColour}${grayColour} La máquina correspondiente para la IP${endColour}${blueColour} $ipAddress${endColour}${grayColour} es${endColour}${purpleColour} $machineName${endColour}\n"
+  if [ "$machineName" ]; then
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} La máquina correspondiente para la IP${endColour}${blueColour} $ipAddress${endColour}${grayColour} es${endColour}${purpleColour} $machineName${endColour}\n"
+  else
+    echo -e "\n${redColour}[!] La dirección IP proporcionada no existe${endColour}"
+  fi
 }
 
+getYoutubeLink(){
+  machineName="$1"
+
+  youtubeLink="$(cat bundle.js | awk "/name: \"${machineName}\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta" | tr -d '"' | tr -d ',' | sed 's/^ *//' | grep youtube | awk 'NF{print $NF}')"
+
+  if [ $youtubeLink ]; then
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} El tutorial para esta máquina está en el siguiente enlace:${endColour}${turquoiseColour} $youtubeLink${endColour}\n"
+  else
+    echo -e "\n${redColour}[!] La máquina proporcionada no existe${endColour}\n"
+  fi
+
+}
+
+getMachinesDifficulty(){
+  difficulty="$1"
+
+  results_check=$(cat bundle.js | grep "dificultad: \"$difficulty\"" -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column)
+  if [ "$results_check" ]; then
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Listando máquinas con dificultad${endColour}${blueColour} $difficulty${endColour}${grayColour}:${endColour}\n"
+    cat bundle.js | grep "dificultad: \"$difficulty\"" -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column
+  else
+    echo -e "\n${redColour}[!] La dificultad indicada no existe${endColour}\n"
+  fi
+}
+
+getOSMachines(){
+  os="$1"
+  
+  os_results=$(cat bundle.js | grep "so: \"Linux\"" -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column)
+
+  if [ "$os_results" ]; then
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Mostrando las máquinas cuyo sistema operativo es ${blueColour}$os${endColour}${grayColour}:${endColour}\n"
+    cat bundle.js | grep "so: \"$os\"" -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column
+  else
+    echo -e "\n${redColour}[!] El sistema operativo indicado no existe${endColour}\n"
+  fi
+}
+
+function getOSDifficultyMachines(){
+  difficulty="$1"
+  os="$2"
+  
+  check_results=$(cat bundle.js | grep "so: \"$os\"" -C 4 | grep "dificultad: \"$difficulty\"" -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column)
+
+  if [ "$check_results" ]; then
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Listando máquinas de dificultad${endColour}${blueColour} $difficulty${endColour}${grayColour} que tengan sistema operativo${endColour}${blueColour} $os${endColour}${grayColour}:${endColour}\n"
+    cat bundle.js | grep "so: \"$os\"" -C 4 | grep "dificultad: \"$difficulty\"" -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column
+  else
+    echo -e "\n${redColour}[!] Se han indicado una dificultad o sistema operativo incorrecto${endColour}\n"
+  fi
+}  
 # Indicadores
 declare -i parameter_counter=0
 
-while getopts "m:ui:h" arg; do
+# Chivatos
+declare -i chivato_difficulty=0
+declare -i chivato_os=0
+
+while getopts "m:ui:y:d:o:h" arg; do
   case $arg in
-    m) machineName=$OPTARG; let parameter_counter+=1;;
+    m) machineName="$OPTARG"; let parameter_counter+=1;;
     u) let parameter_counter+=2;;
-    i) ipAddress=$OPTARG; let parameter_counter+=3;;
+    i) ipAddress="$OPTARG"; let parameter_counter+=3;;
+    y) machineName="$OPTARG"; let parameter_counter+=4;;
+    d) difficulty="$OPTARG"; chivato_difficulty=1; let parameter_counter+=5;;
+    o) os="$OPTARG"; chivato_os=1; let parameter_counter+=6;;
     h) ;;
   esac
 done
@@ -95,6 +166,14 @@ elif [ $parameter_counter -eq 2 ]; then
   updateFiles
 elif [ $parameter_counter -eq 3 ]; then
   searchIP $ipAddress
+elif [ $parameter_counter -eq 4 ]; then
+  getYoutubeLink $machineName
+elif [ $parameter_counter -eq 5 ]; then
+  getMachinesDifficulty $difficulty
+elif [ $parameter_counter -eq 6 ]; then
+  getOSMachines $os
+elif [ $chivato_difficulty -eq 1 ] && [ $chivato_os -eq 1 ]; then
+  getOSDifficultyMachines $difficulty $os
 else
   helpPanel
 fi
